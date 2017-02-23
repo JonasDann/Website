3f188b88631493ca2721126bf2b2542fbe994ee9
Jonas Dann <jonas.chr.dann@gmail.com>
2017-02-23 02:06:29 +0100

    Plugged together stuff to make templates work

--- src/github.com/jonasdann/website/main.go
+++ src/github.com/jonasdann/website/main.go
@@ -1,15 +1,83 @@
 package main
 
 import (
-	"io"
 	"net/http"
+	"html/template"
+	"fmt"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+type CV struct {
+	activities  []Activity
+	Bio         Bio
+	educations  []Education
+	experiences []Experience
+}
+
+type Activity struct {
+	time        string
+	title       string
+	description string
+}
+
+type Bio struct {
+	FirstName     string
+	SecondName    string
+	LastName      string
+	birthDate     string
+	birthLocation string
+	street        string
+	houseNumber   string
+	postalCode    string
+	city          string
+	country       string
+	phone         string
+	email         string
+	languages     map[string]string
+	skills        map[string]map[string]string
+	links         map[string][]string
+}
+
+type Education struct {
+	from               string
+	until              string
+	expectedGraduation string
+	name               string
+	location           string
+	degree             string
+	grade              string
+	description        string
+}
+
+type Experience struct {
+	from        string
+	until       string
+	name        string
+	location    string
+	position    string
+	department  string
+	description string
+	appliedTech []string
+}
+
+type Page struct {
+	title string
+	body  template.HTML
+}
+
+func cv(w http.ResponseWriter, r *http.Request) {
+	cv := *new(CV)
+	cv.Bio = *new(Bio)
+	cv.Bio.FirstName = "Jonas"
+	cv.Bio.SecondName = "Christian"
+	cv.Bio.LastName = "Dann"
+	fmt.Println(cv)
+	tmpl, err1 := template.New("cv").ParseFiles("templates/cv.go.html")
+	fmt.Println(err1)
+	err2 := tmpl.Execute(w, cv)
+	fmt.Println(err2)
 }
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", cv)
 	http.ListenAndServe(":8000", nil)
 }
